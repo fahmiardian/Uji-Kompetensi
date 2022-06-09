0dcc70db64904f92602edbc682471289820262b7
Fahmi Ardiansyah <89506662+fahmiardian@users.noreply.github.com>
2022-06-09 22:26:19 +0700

    Update main.php

--- main.php
+++ main.php
@@ -7,7 +7,6 @@
 	{
 		$mahasiswa = cari($_POST["keyword"]);
 	}
-
 ?>
 
 <!DOCTYPE html>
@@ -17,7 +16,7 @@
     <meta charset="UTF-8">
     <title>Program Informasi</title>
 
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" type="text/css" href="main.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -25,16 +24,18 @@
 </head>
 
 <body>
+    <!-- Start Navbar -->
     <nav>
         <div class="logo">
-            <h2 class="nvr">Portal Akademik</h2>
+            <h2 class="nvr"> Portal Akademik</h2>
         </div>
         
         <ul>
-            <li><a href="" class="nvr">Home</a></li>
-            <li><a href="" class="nvr">Login</a></li>
-            <li><a href="" class="nvr">Main Menu</a></li>
-            <li><a href="" class="nvr">FAQ</a></li>
+            <li><a href="main.php" class="nvr">Home</a></li>
+            <li><a href="login.php" class="nvr">Login</a></li>
+            <li><a href="mahasiswa.php" class="nvr">Data Mahasiswa</a></li>
+            <li><a href="dosen.php" class="nvr">Data Dosen</a></li>
+            <li><a href="" class="nvr">Data TA</a></li>
         </ul>
 
         <div class="menu-toogle">
@@ -45,43 +46,11 @@
            <span></span> 
         </div>
     </nav>
-    <br>
-    <br>
-    <br>
+    <!-- END Navbar -->
 
-    <!-- fungsi search -->
-    <form class="fungsiphp2" action="" method="post">
-		<input type="text" name="keyword" size="40" autofocus placeholder="Cari disini" autocomplete="off">
-		<button type="submit" name="cari" style="margin-left: 5px">Cari</button>
-	</form>
-	<br>
-	<div class="fungsiphp">
-		<table border="1" cellpadding="10" cellspacing="5">
-			<tr>
-				<th>No.</th>
-				<th>NIM</th>
-				<th>Nama</th>
-				<th>Jurusan</th>
-				<th>Semester</th>
-				<th>Alamat</th>
-				<th>No.Telp</th>
-			</tr>
+    <h1 style="display:flex;justify-content: center;margin-top: 10%">Selamat Datang!</h1>
+    <h1 style="display:flex;justify-content: center;margin-top: 1%">Silahkan Pilih Menu Dari Navigation Bar</h1>
 
-			<?php $i= 1;?>
-			<?php foreach($mahasiswa as $row) : ?>
-			<tr>
-				<td><?php echo $i; ?></td>
-				<td><?php echo $row["NIM"] ?></td>
-				<td><?php echo $row["namamahasiswa"] ?></td>
-				<td><?php echo $row["jurusan"] ?></td>
-				<td><?php echo $row["semester"] ?></td>
-				<td><?php echo $row["alamat"] ?></td>
-				<td><?php echo $row["notelp"] ?></td>
-			</tr>
-			<?php $i++; ?>
-			<?php endforeach; ?>
-		</table>
-	</div>
 
     <div class="footer">
     </div>
